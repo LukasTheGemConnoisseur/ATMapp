cfdc23e148dfcd9665f087cbb8446a41538ba441
Lukas Karuza <lukaskar97@gmail.com>
2023-12-18 20:46:04 -0600

    Add project files.

---
+++ ATMapp/UI/AppScreen.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMapp.UI
+{
+    public class AppScreen
+    {
+        internal static void Welcome()
+        {
+            // Clears the console screen
+            Console.Clear();
+
+            // set the title of the console window
+            Console.Title = " Bitcoin ATM App ";
+
+            // Sets the text color or foreground color to white
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Set the welcome message
+            Console.WriteLine("\n\n --------------- Welcome To my Bitcoin ATM APP --------------- \n\n");
+
+            // prompt the user to insert ATM card
+            Console.WriteLine("Please insert your ATM card");
+            Console.WriteLine("Note: The ATM machine will accept and validate " +
+                "a physical ATM card, read the card number and validate it.");
+
+            // We need to add a functionality where the user can continue to the next step
+            Utility.PressEnterToContinue();
+        }
+
+        //Create a method that validates the user's account
+
+
+
+        // Create a method for the LoginProcess
+
+        internal static void LoginProgress()
+        {
+            Console.WriteLine("\nChecking card number and PIN...");
+            //add an animation functionality to show the application is loading/checking
+            Utility.PrintDotAnimation();
+        }
+
+        internal static void PrintLockScreen()
+        {
+            Console.Clear();
+            //write a message stating that the account has been locked
+            Utility.PrintMessage(" Your Account is locked. Please go to the nearest branch to unlock your account. Thank you.", true);
+
+            Environment.Exit(1);
+        }
+
+
+
+
+
+    }
+}
---
+++ ATMapp/UI/Utility.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMapp.UI
+{
+    public static class Utility
+    {
+        public static string GetSecretInput(string prompt)
+        {
+            bool isPrompt = true;
+            string asterisk = "";
+
+            StringBuilder input = new StringBuilder();
+
+            while (true)
+            {
+                if (isPrompt)
+                {
+                    Console.WriteLine(prompt);
+                }
+                isPrompt = false;
+
+                ConsoleKeyInfo inputKey = Console.ReadKey(true);
+
+                if (inputKey.Key == ConsoleKey.Enter)
+                {
+                    if (input.Length == 6)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        PrintMessage("\n Please enter 6 digits.", false);
+                        input.Clear();
+                        isPrompt = true;
+                        continue;
+                    }
+                }
+                if (inputKey.Key == ConsoleKey.Backspace && input.Length > 0)
+                {
+                    input.Remove(input.Length - 1, 1);
+                }
+                else if (inputKey.Key != ConsoleKey.Backspace)
+                {
+                    input.Append(inputKey.KeyChar);
+                    Console.Write(asterisk + "*");
+                }
+            }
+
+            return input.ToString();
+        }
+            public static void PrintMessage(string message, bool success = true) 
+            {
+                if(success)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else 
+                {
+                    Console.ForegroundColor= ConsoleColor.Red;
+                }
+
+                Console.WriteLine(message);
+                Console.ForegroundColor = ConsoleColor.White;
+
+            //insert a method call for the next screen or next step  (enter)
+            PressEnterToContinue();
+            }
+
+        public static string GetUserInput (string prompt)
+        {
+            Console.WriteLine($"Enter {prompt}");
+            return Console.ReadLine();
+        }
+
+        public static void PrintDotAnimation (int timer = 10)
+        {
+            for(int i = 0; i < timer; i++) 
+            {
+                Console.Write(".");
+                Thread.Sleep(200); // Thread means a linear process that's being executed
+            }
+            Console.Clear();
+        }
+
+        public static void PressEnterToContinue()
+        {
+            Console.WriteLine("\n\nPress Enter to Continue... \n");
+            Console.ReadLine();
+        }
+        
+
+
+    }
+}
